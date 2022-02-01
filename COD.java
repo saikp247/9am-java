106ad0a6591fb3b0e3935fef14b49a259b1bdd83
Sai <sai.punjal247@gmail.com>
2022-02-01 18:28:22 +0530

    COD implemented

--- COD.java
+++ COD.java
@@ -1,5 +1,6 @@
 class COD {  
     public static void main(String args[]){  
-     System.out.println("Implement COD functionality");  
+     System.out.println("Implement COD functionality");
+	 System.out.println("Implemented COD functionality");
     }  
 }  
